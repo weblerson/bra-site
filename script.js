208269d0602b5559e56620221af4650b6ef71ab2
weblerson <zonejr4@gmail.com>
2022-10-03 10:16:09 -0400

    scroll start/obj

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function scrollToId(id) {
     const element = document.querySelector('#' + id);
     element.scrollIntoView({behavior: 'smooth'}, true);
 }
+
+function scrollToStart() {
+    window.scrollTo(0, 0);
+}
+
+function scrollToWho() {
+    window.innerWidth >= 1440? window.scrollTo(0, 1087) : 
+    window.innerWidth >= 1024? window.scrollTo(0, 1290) :
+    window.innerWidth >= 768? window.scrollTo(0, 1700) :
+    window.innerWidth >= 425? window.scrollTo(0, 2096) :
+    window.innerWidth >= 375? window.scrollTo(0, 2389) :
+    window.innerWidth >= 320? window.scrollTo(0, 2750) : ':)';
+}
+
+function scrollToObj() {
+    window.innerWidth >= 1440? window.scrollTo(0, 1087) : 
+    window.innerWidth >= 1024? window.scrollTo(0, 1290) :
+    window.innerWidth >= 768? window.scrollTo(0, 1700) :
+    window.innerWidth >= 425? window.scrollTo(0, 2096) :
+    window.innerWidth >= 375? window.scrollTo(0, 2389) :
+    window.innerWidth >= 320? window.scrollTo(0, 2750) : ':)';
+}
