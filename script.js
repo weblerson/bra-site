919e032d5c0c69b294f0f97f27c6ed51185d5617
weblerson <zonejr4@gmail.com>
2022-10-15 20:36:55 -0400

    some changes on obj

--- script.js
+++ script.js
@@ -15,10 +15,18 @@ function scrollToWho() {
     window.innerWidth >= 375? window.scrollTo(0, 396) : window.scrollTo(0, 395);
 }
 
-function scrollToObj() {
-    window.innerWidth >= 1440? window.scrollTo(0, 1087) : 
-    window.innerWidth >= 1024? window.scrollTo(0, 1290) :
-    window.innerWidth >= 768? window.scrollTo(0, 1700) :
-    window.innerWidth >= 425? window.scrollTo(0, 2442) :
-    window.innerWidth >= 375? window.scrollTo(0, 2566) : window.scrollTo(0, 3096);
+function scrollToPremens() {
+    window.innerWidth >= 1440? window.scrollTo(0, 1100) : 
+    window.innerWidth >= 1024? window.scrollTo(0, 1250) :
+    window.innerWidth >= 768? window.scrollTo(0, 1758) :
+    window.innerWidth >= 425? window.scrollTo(0, 2496) :
+    window.innerWidth >= 375? window.scrollTo(0, 2781) : window.scrollTo(0, 3141);
+}
+
+function scrollToTorreifer() {
+    window.innerWidth >= 1440? window.scrollTo(0, 1537) : 
+    window.innerWidth >= 1024? window.scrollTo(0, 2006) :
+    window.innerWidth >= 768? window.scrollTo(0, 2484) :
+    window.innerWidth >= 425? window.scrollTo(0, 3582) :
+    window.innerWidth >= 375? window.scrollTo(0, 4056) : window.scrollTo(0, 4555);
 }
